60a2533398b16c3cd655ada3ee6269b11cf7c47f
Nickshout <fel_andrade@hotmail.com>
2022-05-05 22:16:04 -0300

    nlw

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -31,7 +31,7 @@ const feedbackTypes = {
 type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-    const [feedbackTipe, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
   return (
     <div className=" bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -39,21 +39,29 @@ export function WidgetForm() {
 
         <CloseButton />
       </header>
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-            key={key}
-              className=" bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setFeedbackType(key)}
-              type="button"
-            >
-              <img src={value.image.source} alt={value.image.alt}></img>
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+
+    {!feedbackType ? (
+            <div className="flex py-8 gap-2 w-full">
+            {Object.entries(feedbackTypes).map(([key, value]) => {
+              return (
+                <button
+                key={key}
+                  className=" bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                  onClick={() => setFeedbackType(key as FeedbackType)}
+                  type="button"
+                >
+                  <img src={value.image.source} alt={value.image.alt}></img>
+                  <span>{value.title}</span>
+                </button>
+              );
+            })}
+          </div>
+    ) : (
+      <p>Hello World</p>
+    )
+
+    }
+
 
       <footer className="text-xs text-neutral-400">
         Feito por{" "}
